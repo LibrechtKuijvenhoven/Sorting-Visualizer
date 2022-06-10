9057276ed70ee469752b9c8c6a3db7b6bf7009c0
LibrechtKuijvenhoven <librecht@live.nl>
2022-06-10 12:28:44 +0200

    setup for quicksort

---
+++ src/algorithms/QuickSort.ts
@@ -0,0 +1,13 @@
+import { Isteps, SortingAlgorithm } from "./algorithm";
+
+export const QuickSort = (array:number[], steps: Isteps[]) => {
+    
+}
+
+export const QuickSortAlgorithm: SortingAlgorithm = {
+    bestCase: "n log n",
+    average: "n log n",
+    worstCase: "n^2",
+    memory: "log n",
+    name: "Quick Sort"
+}
